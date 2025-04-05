bf6d19a2ae9282ec0716e82eae7e2bc1b260aa3d
KisekiMiracle <luisazaldegui99@gmail.com>
2025-04-05 08:58:50 -0500

    feat: implemented ToS with MDX and lack of sleep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkToc from "remark-toc";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [
+      tailwindcss(),
+      {
+        name: "reload", // via: https://stackoverflow.com/questions/77461040/can-i-get-vite-to-reload-the-browser-on-every-html-change
+        configureServer(server) {
+          const { ws, watcher } = server;
+          watcher.on("change", (file) => {
+            if (file.endsWith(".astro") || file.endsWith(".md") || file.endsWith(".mdx") || file.endsWith(".tsx")) {
+              ws.send({
+                type: "full-reload",
+              });
+            }
+          });
+        },
+      },
+    ],
+  },
+
+  markdown: {
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeHeadingIds],
+  },
+
+  integrations: [mdx()],
 });
